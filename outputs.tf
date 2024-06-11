11632e823ea0fcc2c541023f3a0aa59f7a334cc9
Damien Plénard <damien@plenard.me>
2024-06-12 01:08:41 +0200

    feat: output IP addresses

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "url" {
   description = "The URL of the Coolify instance"
   value       = "http://${hcloud_server.coolify.ipv4_address}:8000"
 }
+
+output "ipv4_address" {
+  description = "The IP of the Coolify instance"
+  value       = hcloud_server.coolify.ipv4_address
+}
+
+output "ipv6_address" {
+  description = "The IPv6 of the Coolify instance"
+  value       = hcloud_server.coolify.ipv6_address
+}
