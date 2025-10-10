ee3ff131b624ef62105a9006fe3fcce2e7712cc8
Damien Plénard <damien@plenard.me>
2025-10-10 21:43:52 +0200

    chore(deps): remove provider version lock

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = ">= 1.8.4"
   required_providers {
     hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = "1.54.0"
+      source = "hetznercloud/hcloud"
     }
   }
 }
